8bdc301807ef7835cff9531e66deff1a8415165b
LucGamesYT <beckerluca16@gmail.com>
2021-02-01 23:26:50 +0100

    Implement snow layer "logic"

--- src/main/java/org/jukeboxmc/block/BlockSnowLayer.java
+++ src/main/java/org/jukeboxmc/block/BlockSnowLayer.java
@@ -1,5 +1,12 @@
 package org.jukeboxmc.block;
 
+import org.jukeboxmc.block.direction.BlockFace;
+import org.jukeboxmc.item.Item;
+import org.jukeboxmc.math.BlockPosition;
+import org.jukeboxmc.math.Vector;
+import org.jukeboxmc.player.Player;
+import org.jukeboxmc.world.World;
+
 /**
  * @author LucGamesYT
  * @version 1.0
@@ -8,6 +15,23 @@ public class BlockSnowLayer extends Block {
 
     public BlockSnowLayer() {
         super( "minecraft:snow_layer" );
+    }
+
+    @Override
+    public boolean placeBlock( Player player, World world, BlockPosition blockPosition, BlockPosition placePosition, Vector clickedPosition, Item itemIndHand, BlockFace blockFace ) {
+        Block block = world.getBlock( blockPosition );
+
+        if ( block instanceof BlockSnowLayer ) {
+            BlockSnowLayer blockSnowLayer = (BlockSnowLayer) block;
+            if ( blockSnowLayer.getHeight() != 7 ) {
+                this.setHeight( blockSnowLayer.getHeight() + 1 );
+                world.setBlock( blockPosition, this );
+            }
+        } else {
+            this.setHeight( 0 );
+            world.setBlock( placePosition, this );
+        }
+        return true;
     }
 
     public void setCovered( boolean value ) {
