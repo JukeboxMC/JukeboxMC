7a47e1764cab827379f814c85a1d09274b4615f9
LucGamesYT <beckerluca16@gmail.com>
2021-04-05 17:42:51 +0200

    Implement trapped chest direction

--- src/main/java/org/jukeboxmc/block/BlockTrappedChest.java
+++ src/main/java/org/jukeboxmc/block/BlockTrappedChest.java
@@ -1,6 +1,11 @@
 package org.jukeboxmc.block;
 
 import org.jukeboxmc.block.direction.BlockFace;
+import org.jukeboxmc.item.Item;
+import org.jukeboxmc.math.BlockPosition;
+import org.jukeboxmc.math.Vector;
+import org.jukeboxmc.player.Player;
+import org.jukeboxmc.world.World;
 
 /**
  * @author LucGamesYT
@@ -10,6 +15,13 @@ public class BlockTrappedChest extends Block {
 
     public BlockTrappedChest() {
         super( "minecraft:trapped_chest" );
+    }
+
+    @Override
+    public boolean placeBlock( Player player, World world, BlockPosition blockPosition, BlockPosition placePosition, Vector clickedPosition, Item itemIndHand, BlockFace blockFace ) {
+        this.setBlockFace( player.getDirection().toBlockFace().opposite() );
+        world.setBlock( placePosition, this );
+        return true;
     }
 
     public void setBlockFace( BlockFace blockFace ) {
