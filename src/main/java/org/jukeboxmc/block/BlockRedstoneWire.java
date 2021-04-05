021affc9fcde93b4f43bad6f279ae4dbbdb1ee4e
LucGamesYT <beckerluca16@gmail.com>
2021-04-05 18:03:05 +0200

    Set redstone signal default value 0

--- src/main/java/org/jukeboxmc/block/BlockRedstoneWire.java
+++ src/main/java/org/jukeboxmc/block/BlockRedstoneWire.java
@@ -1,5 +1,12 @@
 package org.jukeboxmc.block;
 
+import org.jukeboxmc.block.direction.BlockFace;
+import org.jukeboxmc.item.Item;
+import org.jukeboxmc.math.BlockPosition;
+import org.jukeboxmc.math.Vector;
+import org.jukeboxmc.player.Player;
+import org.jukeboxmc.world.World;
+
 /**
  * @author LucGamesYT
  * @version 1.0
@@ -8,6 +15,13 @@ public class BlockRedstoneWire extends Block {
 
     public BlockRedstoneWire() {
         super( "minecraft:redstone_wire" );
+    }
+
+    @Override
+    public boolean placeBlock( Player player, World world, BlockPosition blockPosition, BlockPosition placePosition, Vector clickedPosition, Item itemIndHand, BlockFace blockFace ) {
+        this.setRedstoneSignal( 0 );
+        world.setBlock( placePosition, this );
+        return true;
     }
 
     public void setRedstoneSignal( int value ) {
