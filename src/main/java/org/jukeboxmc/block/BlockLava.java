a6ac4d4cb11b7e8c77cc8de98e3a567c4a73b57d
LucGamesYT <beckerluca16@gmail.com>
2021-06-06 16:35:14 +0200

    Flowing lava is here!

--- src/main/java/org/jukeboxmc/block/BlockLava.java
+++ src/main/java/org/jukeboxmc/block/BlockLava.java
@@ -1,15 +1,29 @@
 package org.jukeboxmc.block;
 
+import org.jukeboxmc.block.direction.BlockFace;
+import org.jukeboxmc.item.Item;
 import org.jukeboxmc.item.ItemLava;
+import org.jukeboxmc.math.Location;
+import org.jukeboxmc.math.Vector;
+import org.jukeboxmc.player.Player;
+import org.jukeboxmc.world.Dimension;
+import org.jukeboxmc.world.World;
 
 /**
  * @author LucGamesYT
  * @version 1.0
  */
-public class BlockLava extends Block {
+public class BlockLava extends BlockLiquid {
 
     public BlockLava() {
         super( "minecraft:lava" );
+    }
+
+    @Override
+    public boolean placeBlock( Player player, World world, Vector blockPosition, Vector placePosition, Vector clickedPosition, Item itemIndHand, BlockFace blockFace ) {
+        world.setBlock( placePosition, this, 0, false );
+        world.scheduleBlockUpdate( new Location( world, placePosition ), this.getTickRate() );
+        return true;
     }
 
     @Override
@@ -22,11 +36,23 @@ public class BlockLava extends Block {
         return BlockType.LAVA;
     }
 
-    public void setLiquidDepth( int value ) {
-        this.setState( "liquid_depth", value );
+    @Override
+    public boolean canBeReplaced( Block block ) {
+        return true;
     }
 
-    public int getLiquidDepth() {
-        return this.stateExists( "liquid_depth" ) ? this.getIntState( "liquid_depth" ) : 0;
+    @Override
+    public int getTickRate() {
+        if ( this.location.getDimension() == Dimension.NETHER ) {
+            return 10;
+        }
+        return 30;
+    }
+
+    @Override
+    public BlockLiquid getBlock( int liquidDepth ) {
+        BlockLava blockLava = new BlockLava();
+        blockLava.setLiquidDepth( liquidDepth );
+        return blockLava;
     }
 }
--- src/main/java/org/jukeboxmc/item/ItemLavaBucket.java
+++ src/main/java/org/jukeboxmc/item/ItemLavaBucket.java
@@ -1,5 +1,7 @@
 package org.jukeboxmc.item;
 
+import org.jukeboxmc.block.BlockLava;
+
 /**
  * @author LucGamesYT
  * @version 1.0
@@ -10,5 +12,8 @@ public class ItemLavaBucket extends Item {
         super( 363 );
     }
 
-
+    @Override
+    public BlockLava getBlock() {
+        return new BlockLava();
+    }
 }
