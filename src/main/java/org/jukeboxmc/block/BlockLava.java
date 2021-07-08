badf160762d7cb8eea3cba4098d84bd4331067c4
LucGamesYT <beckerluca16@gmail.com>
2021-07-08 16:25:01 +0200

    Implement cobblestone generator

--- src/main/java/org/jukeboxmc/block/BlockLava.java
+++ src/main/java/org/jukeboxmc/block/BlockLava.java
@@ -1,5 +1,6 @@
 package org.jukeboxmc.block;
 
+import org.jukeboxmc.block.direction.BlockFace;
 import org.jukeboxmc.item.ItemLava;
 import org.jukeboxmc.world.Dimension;
 
@@ -11,6 +12,10 @@ public class BlockLava extends BlockLiquid {
 
     public BlockLava() {
         super( "minecraft:lava" );
+    }
+
+    protected BlockLava( String identifier ) {
+        super( identifier );
     }
 
     @Override
@@ -29,11 +34,48 @@ public class BlockLava extends BlockLiquid {
     }
 
     @Override
+    public int getFlowDecayPerBlock() {
+        if ( this.location.getDimension() == Dimension.NETHER ) {
+            return 1;
+        }
+        return 2;
+    }
+
+    @Override
     public int getTickRate() {
         if ( this.location.getDimension() == Dimension.NETHER ) {
             return 10;
         }
         return 30;
+    }
+
+    @Override
+    protected void checkForHarden() {
+        Block colliding = null;
+        for ( BlockFace value : BlockFace.values() ) {
+            if ( value == BlockFace.DOWN ) continue;
+            Block blockSide = this.getSide( value );
+            if ( blockSide instanceof BlockWater ) {
+                colliding = blockSide;
+                break;
+            }
+        }
+        if ( colliding != null ) {
+            if ( this.getLiquidDepth() == 0 ) {
+                this.liquidCollide( new BlockObsidian() );
+            } else if ( this.getLiquidDepth() <= 4 ) {
+                this.liquidCollide( new BlockCobblestone() );
+            }
+        }
+    }
+
+    @Override
+    protected void flowIntoBlock( Block block, int newFlowDecay ) {
+        if ( block instanceof BlockWater ) {
+            ( (BlockLiquid) block ).liquidCollide( new BlockStone() );
+        } else {
+            super.flowIntoBlock( block, newFlowDecay );
+        }
     }
 
     @Override
