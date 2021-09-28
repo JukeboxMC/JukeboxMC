907de6f66aa109056e3101ae4a0172e7a45d12c0
LucGamesYT <Amybecker1998>
2021-09-28 10:11:50 +0200

    Implement PlayerCommandPreprocessEvent

--- src/main/java/org/jukeboxmc/network/handler/CommandRequestHandler.java
+++ src/main/java/org/jukeboxmc/network/handler/CommandRequestHandler.java
@@ -1,6 +1,7 @@
 package org.jukeboxmc.network.handler;
 
 import org.jukeboxmc.Server;
+import org.jukeboxmc.event.player.PlayerCommandPreprocessEvent;
 import org.jukeboxmc.network.packet.CommandRequestPacket;
 import org.jukeboxmc.player.Player;
 
@@ -13,6 +14,14 @@ public class CommandRequestHandler implements PacketHandler<CommandRequestPacket
     @Override
     public void handle( CommandRequestPacket packet, Server server, Player player ) {
         try {
+            final String[] commandParts = packet.getInputCommand().substring( 1 ).split( " " );
+            final String commandIdentifier = commandParts[0];
+            PlayerCommandPreprocessEvent playerCommandPreprocessEvent = new PlayerCommandPreprocessEvent( player, commandIdentifier );
+            server.getPluginManager().callEvent( playerCommandPreprocessEvent );
+
+            if ( playerCommandPreprocessEvent.isCancelled() ) {
+                return;
+            }
             server.getPluginManager().getCommandManager().handleCommandInput( player, packet.getInputCommand() );
         } catch ( Throwable e ) {
             e.printStackTrace();
