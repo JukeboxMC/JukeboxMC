908a2422d051b18a19ec4d21c0a8f1498d7f1dc7
LucGamesYT <beckerluca16mail.com>
2021-10-07 13:05:39 +0200

    Handle the new subChunkVersion 9

--- src/main/java/org/jukeboxmc/world/leveldb/LevelDBChunk.java
+++ src/main/java/org/jukeboxmc/world/leveldb/LevelDBChunk.java
@@ -36,6 +36,42 @@ public class LevelDBChunk {
         byte subchunkVersion = buffer.readByte();
         int storages = 1;
         switch ( subchunkVersion ) {
+            case 9:
+                storages = buffer.readByte();
+                buffer.readByte();
+                for ( int layer = 0; layer < storages; layer++ ) {
+                    byte data = buffer.readByte();
+                    boolean isPersistent = ( ( data >> 8 ) & 1 ) != 1;
+                    byte wordTemplate = (byte) ( data >>> 1 );
+
+                    Palette palette = new Palette( buffer, wordTemplate, true );
+                    short[] indexes = palette.getIndexes();
+                    int needed = buffer.readLInt();
+
+                    Map<Short, Integer> chunkPalette = new HashMap<>();
+                    short i = 0;
+                    NBTInputStream reader = NbtUtils.createReaderLE( new ByteBufInputStream( buffer.getBuffer() ) );
+                    while ( i < needed ) {
+                        try {
+                            NbtMap compound = (NbtMap) reader.readTag();
+                            String identifier = compound.getString( "name" );
+                            NbtMap states = compound.getCompound( "states" );
+                            if ( states != null ) {
+                                chunkPalette.put( i++, BlockPalette.getRuntimeId( identifier, states ) );
+                            } else {
+                                chunkPalette.put( i++, BlockPalette.getRuntimeId( identifier, NbtMap.EMPTY ) );
+                            }
+                        } catch ( IOException e ) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+
+                    for ( short index = 0; index < indexes.length; index++ ) {
+                        chunk.blocks[layer][index] = chunkPalette.get( indexes[index] );
+                    }
+                }
+                break;
             case 8:
                 storages = buffer.readByte();
             case 1:
