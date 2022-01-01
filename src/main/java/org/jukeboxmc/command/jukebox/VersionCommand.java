e0f8002054cbe2a539eb7a70f9c78a6015b44bda
Vazzi <45903049+VazziDE@users.noreply.github.com>
2022-01-01 22:33:13 +0100

    Update VersionCommand.java

--- src/main/java/org/jukeboxmc/command/jukebox/VersionCommand.java
+++ src/main/java/org/jukeboxmc/command/jukebox/VersionCommand.java
@@ -28,27 +28,30 @@ public class VersionCommand extends Command {
             sendServerInfo(commandSender);
             return;
         }
-
         if (Server.getInstance().getPluginManager().isPluginLoaded(args[0])) {
             sendPluginInfo(commandSender, Server.getInstance().getPluginManager().getPluginByName(args[0]));
-        }else{
+        } else {
             commandSender.sendMessage("Plugin " + args[0] + " not found.");
         }
 
     }
 
     private void sendServerInfo(CommandSender commandSender) {
+
         commandSender.sendMessage("§fJukeboxMC MCBE Server Software");
         commandSender.sendMessage("§7Software Version: §e" + JukeboxMC.version);
         commandSender.sendMessage("§7Minecraft Version: §e" + Protocol.MINECRAFT_VERSION);
         commandSender.sendMessage("§7Current Protocol: §e" + Protocol.CURRENT_PROTOCOL);
+
     }
 
     private void sendPluginInfo(CommandSender commandSender, Plugin plugin) {
+
         commandSender.sendMessage("§fPlugin Info §7- §a" + plugin.getName());
         commandSender.sendMessage("§7Name: §e" + plugin.getName());
         commandSender.sendMessage("§7Version: §e" + plugin.getVersion());
         commandSender.sendMessage("§7Author: §e" + plugin.getAuthor());
+
     }
 
 }
