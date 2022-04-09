c1fd43504096abcad09c86a1c176abf00315efe6
Luca <lucdev@potionstudios.de>
2022-04-09 18:01:43 +0200

    Add SayCommand (without testing)

---
+++ src/main/java/org/jukeboxmc/command/jukebox/SayCommand.java
@@ -0,0 +1,41 @@
+package org.jukeboxmc.command.jukebox;
+
+import org.jukeboxmc.command.Command;
+import org.jukeboxmc.command.CommandSender;
+import org.jukeboxmc.command.annotation.Description;
+import org.jukeboxmc.command.annotation.Name;
+import org.jukeboxmc.command.annotation.Permission;
+import org.jukeboxmc.console.ConsoleSender;
+import org.jukeboxmc.player.Player;
+
+/**
+ * @author Luca W. | GPS_Gamer
+ **/
+
+@Name("say")
+@Description("Send a message to all online players.")
+@Permission("jukebox.command.say")
+public class SayCommand extends Command {
+
+    @Override
+    public void execute( CommandSender sender, String command, String[] args ) {
+       String senderString;
+       if ( sender instanceof Player ) {
+           senderString = ((Player) sender).getName();
+       } else if ( sender instanceof ConsoleSender ) {
+           senderString = "Server";
+       } else {
+           senderString = "Unknown";
+       }
+
+       StringBuilder sayMessage = new StringBuilder();
+       for ( String arg : args ) {
+           sayMessage.append(arg).append(" ");
+       }
+       if ( sayMessage.length() > 0 ) {
+           sayMessage.substring(0, sayMessage.length() - 1);
+       }
+
+       sender.getServer().broadcastMessage( "[" + senderString + "] " + sayMessage );
+    }
+}
