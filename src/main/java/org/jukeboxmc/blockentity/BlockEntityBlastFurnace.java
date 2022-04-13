6ea8a2d51c6736da98df447e78bc25522aa35d21
LucGamesYT <Amybecker1998>
2022-04-13 08:17:00 +0200

    Implement blast furnace saving

--- src/main/java/org/jukeboxmc/blockentity/BlockEntityBlastFurnace.java
+++ src/main/java/org/jukeboxmc/blockentity/BlockEntityBlastFurnace.java
@@ -6,7 +6,13 @@ import org.jukeboxmc.inventory.BlastFurnaceInventory;
 import org.jukeboxmc.inventory.InventoryHolder;
 import org.jukeboxmc.item.Item;
 import org.jukeboxmc.math.Vector;
+import org.jukeboxmc.nbt.NbtMap;
+import org.jukeboxmc.nbt.NbtMapBuilder;
+import org.jukeboxmc.nbt.NbtType;
 import org.jukeboxmc.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author LucGamesYT
@@ -26,6 +32,41 @@ public class BlockEntityBlastFurnace extends SmeltingComponent implements Invent
     public boolean interact( Player player, Vector blockPosition, Vector clickedPosition, BlockFace blockFace, Item itemInHand ) {
         player.openInventory( this.blastFurnaceInventory, blockPosition );
         return super.interact( player, blockPosition, clickedPosition, blockFace, itemInHand );
+    }
+
+    @Override
+    public void fromCompound( NbtMap compound ) {
+        super.fromCompound( compound );
+
+        List<NbtMap> items = compound.getList( "Items", NbtType.COMPOUND );
+        for ( NbtMap nbtMap : items ) {
+            Item item = this.toItem( nbtMap );
+            byte slot = nbtMap.getByte( "Slot", (byte) 127 );
+            if ( slot == 127 ) {
+                this.blastFurnaceInventory.addItem( item, false );
+            } else {
+                this.blastFurnaceInventory.setItem( slot, item, false );
+            }
+        }
+    }
+
+    @Override
+    public NbtMapBuilder toCompound() {
+        NbtMapBuilder builder = super.toCompound();
+
+        List<NbtMap> itemsCompoundList = new ArrayList<>();
+        for ( int slot = 0; slot < this.blastFurnaceInventory.getSize(); slot++ ) {
+            NbtMapBuilder itemCompound = NbtMap.builder();
+            Item item = this.blastFurnaceInventory.getItem( slot );
+
+            itemCompound.putByte( "Slot", (byte) slot );
+            this.fromItem( item, itemCompound );
+
+            itemsCompoundList.add( itemCompound.build() );
+        }
+
+        builder.putList( "Items", NbtType.COMPOUND, itemsCompoundList );
+        return builder;
     }
 
     public BlastFurnaceInventory getBlastFurnaceInventory() {
