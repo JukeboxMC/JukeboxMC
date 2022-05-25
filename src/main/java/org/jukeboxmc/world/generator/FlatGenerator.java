928803978a6a44430c45863d1e39db1b153c7315
LucGamesYT <Amybecker1998>
2022-05-25 12:24:55 +0200

    Revert flat generator

--- src/main/java/org/jukeboxmc/world/generator/FlatGenerator.java
+++ src/main/java/org/jukeboxmc/world/generator/FlatGenerator.java
@@ -13,24 +13,15 @@ import org.jukeboxmc.world.chunk.Chunk;
  */
 public class FlatGenerator extends Generator {
 
-    private final Block blockStone;
     private final Block blockGrass;
     private final Block blockDirt;
     private final Block blockBedrock;
-    private final Block blockRedstone;
-    private final Block blockLapis;
-    private final Block blockEmerald;
 
     public FlatGenerator( World world, Dimension dimension ) {
         super( world, dimension );
-
-        this.blockStone = new BlockStone();
         this.blockGrass = new BlockGrass();
         this.blockDirt = new BlockDirt();
         this.blockBedrock = new BlockBedrock();
-        this.blockRedstone = new BlockRedstoneBlock();
-        this.blockLapis = new BlockLapisBlock();
-        this.blockEmerald = new BlockEmeraldBlock();
     }
 
     @Override
@@ -42,45 +33,21 @@ public class FlatGenerator extends Generator {
                     chunk.setBiome( blockX, blockY, blockZ, Biome.PLAINS );
                 }
 
-                chunk.setBlock( blockX, 0, blockZ, 0, this.blockStone );
-                chunk.setBlock( blockX, 1, blockZ, 0, this.blockStone );
-                chunk.setBlock( blockX, 2, blockZ, 0, this.blockStone );
-                chunk.setBlock( blockX, 3, blockZ, 0, this.blockStone );
-            }
-        }
-    }
-
-    @Override
-    public void populate( int chunkX, int chunkZ ) {
-        Chunk chunk = this.getChunk( chunkX, chunkZ );
-        for ( int blockX = 0; blockX < 16; blockX++ ) {
-            for ( int blockZ = 0; blockZ < 16; blockZ++ ) {
                 chunk.setBlock( blockX, 0, blockZ, 0, this.blockBedrock );
                 chunk.setBlock( blockX, 1, blockZ, 0, this.blockDirt );
                 chunk.setBlock( blockX, 2, blockZ, 0, this.blockDirt );
                 chunk.setBlock( blockX, 3, blockZ, 0, this.blockGrass );
             }
         }
+    }
 
-        Chunk chunkLeft = this.getChunk( chunkX + 1, chunkZ );
+    @Override
+    public void populate( int chunkX, int chunkZ ) {
 
-        chunkLeft.setBlock( 0, 8, 0, 0, this.blockBedrock );
-
-        Chunk chunkRight = this.getChunk( chunkX - 1, chunkZ );
-
-        chunkRight.setBlock( 0, 9, 0, 0, this.blockRedstone );
-
-        Chunk chunkUp = this.getChunk( chunkX, chunkZ + 1 );
-
-        chunkUp.setBlock( 0, 10, 0, 0, this.blockLapis );
-
-        Chunk chunkDown = this.getChunk( chunkX, chunkZ - 1 );
-
-        chunkDown.setBlock( 0, 11, 0, 0, this.blockEmerald );
     }
 
     @Override
     public Vector getSpawnLocation() {
-        return new Vector( 0, 5, 0 );
+        return new Vector( 0.5f, 5, 0.5f );
     }
 }
